eb30d1334f084b403b40ebfa43ebde17f5ae3bdd
Konrad <xinus01@gmail.com>
2024-09-17 22:46:53 +0200

    Adding randomized position for tests

--- load_cached_data.py
+++ load_cached_data.py
@@ -1,6 +1,12 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+
 import unmarshaller
 from delta.vollib import calculate_deltas
 from financial_instruments_to_data_frame import financial_instruments_to_dataframe
+from model.FinancialInstrument import FinancialInstrument
 from penalty import calculate_penalty
 from utilities.dataframe_compact import compact
 
@@ -8,11 +14,13 @@ from utilities.dataframe_compact import compact
 with open('/home/elentirald/PycharmProjects/RITS_penalty/securities.json', 'r', encoding='utf-8') as file:
     json = file.read()
 
-financial_instruments = unmarshaller.unmarshall_to_financial_instruments(json)
+financial_instruments: List[FinancialInstrument] = unmarshaller.unmarshall_to_financial_instruments(json)
+dataframe: pd.DataFrame = financial_instruments_to_dataframe(financial_instruments)
 
-dataframe = financial_instruments_to_dataframe(financial_instruments)
+dataframe['position'] = np.random.randint(-10_000, 10_000, size=len(dataframe))
 
-compacted_dataframe = compact(dataframe)
-deltas = calculate_deltas(dataframe)
-penalty = calculate_penalty(deltas)
+# compacted_dataframe: pd.DataFrame = compact(dataframe)
+deltas: pd.DataFrame = calculate_deltas(dataframe)
+penalty: float = calculate_penalty(deltas)
+
 print(deltas)
--- penalty.py
+++ penalty.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def calculate_penalty(df: pd.DataFrame, delta_limit: float = 1_000, delta_penalty_in_percentage: float = 0.5) -> float:
-    delta_sum = abs(df['delta'].sum())
+    portfolio_delta = df['delta'] * df['position']
+    delta_sum = abs(portfolio_delta.sum())
 
     return 0 if delta_sum <= delta_limit else (delta_sum - delta_limit) * 0.01 * delta_penalty_in_percentage
